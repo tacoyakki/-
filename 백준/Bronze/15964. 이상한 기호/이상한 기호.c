5ca6f4bda647965bab1fa97e2f5529bee29163a3
yun june <wnslfjqm09@naver.com>
2025-10-23 16:19:32 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.c
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int A, B;
-    scanf("%d %d", &A, &B);
-    printf("%d",(A+B)*(A-B));
+    long long A, B, a;
+    
+    scanf("%lld %lld", &A, &B);
+    a = (A + B) * (A - B);
+    printf("%lld",a);
     return 0;
 }
