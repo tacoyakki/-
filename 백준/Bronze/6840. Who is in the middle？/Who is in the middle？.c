b84f0b5192b001fd9ac875f71006e1b8194336b3
yun june <wnslfjqm09@naver.com>
2025-11-03 14:21:23 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a<b&&b<c||a>b&&b>c)
+        printf("%d",b);
+    else if(a>b&&a<c||a<b&&a>c)
+        printf("%d",a);
+    else
+        printf("%d",c);
+    return 0;
+}
